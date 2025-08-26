4b4f688ec0732f0eeee4185e7caf0ee7d82e4953
Shidhin Varghese Philip <ShidhinVarghesePhilip98@gmail.com>
2025-08-26 13:36:46 -0500

    Updated Testing For SQL

--- src/getSQL.py
+++ src/getSQL.py
@@ -53,7 +53,7 @@ async def stream_answer(query_input:QueryInput):
                 print(f"Checker: {isinstance(msg,AIMessage)}")
                 if  isinstance(msg,AIMessage):
                     print(f"Message Conntent: {msg.content}")
-                    print(f"Message SQL Content: {msg.additional_kwargs["tool_calls"]}")
+                    print(f"Message SQL Content: {msg.additional_kwargs.tool_calls}")
                     # yield msg.content
                     yield (json.dumps({"answer":msg.content})+"\n").encode("utf-8")
 
