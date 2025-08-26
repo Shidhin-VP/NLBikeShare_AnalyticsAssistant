05d66302febcbb9b7eb9f654fdced969a4a8b658
Shidhin Varghese Philip <ShidhinVarghesePhilip98@gmail.com>
2025-08-25 21:06:07 -0500

    Updated Code and Working Model

--- src/getSQL.py
+++ src/getSQL.py
@@ -5,6 +5,7 @@ from langchain_community.chat_models import ChatOllama
 from langchain.schema import AIMessage
 from langchain import hub
 from langgraph.prebuilt import create_react_agent
+from guardrail import guard
 from .connectDB import connectDB, loadAPI, loadURL
 
 from fastapi import FastAPI
@@ -12,6 +13,16 @@ from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 # from typing import Any, List, Optional
 import json
+
+@guard
+def user_question():
+    """
+    Expected user question:
+    - A natural language string
+    - Must NOT contain SQL keywords like DROP, DELETE, INSERT, UPDATE
+    - Max length 200 characters
+    """
+    pass
 
 cDB=connectDB()
 url=loadURL()
@@ -44,6 +55,11 @@ async def root():
 @app.post("/query")
 async def stream_answer(query_input:QueryInput):
     question=query_input.question
+    try:
+        validate=user_question.parse(question)
+        print("Validate: ",validate)
+    except Exception as e: 
+        print(f"Input Validation failed: {e}")
     def even_stream():
         for event in sql_agent.stream({"messages":("user",question)},stream_mode='values'):
                 # last_msg=event['messages'][-1]
